d9f155bc01754bb52e68ff52dcdcef09a31e70d9
CAT <cavilest@gmail.com>
2023-07-10 22:16:36 -0400

    Ejercicio individual 06 - Creación de presentador. - Creación de interface.

---
+++ app/src/main/java/com/example/a06modelviewpresenterexample/IViewPresenter.java
@@ -0,0 +1,8 @@
+package com.example.a06modelviewpresenterexample;
+
+public interface IViewPresenter {
+    public void showWeakPassword();
+    public void showMediumPassword();
+    public void showStrongPassword();
+    public void showErrorMessage();
+}
---
+++ app/src/main/java/com/example/a06modelviewpresenterexample/PasswordPresenter.java
@@ -0,0 +1,33 @@
+package com.example.a06modelviewpresenterexample;
+
+public class PasswordPresenter {
+
+    private PasswordModel passwordModel;
+
+
+    private IViewPresenter iViewPresenter;
+
+    public PasswordPresenter(IViewPresenter iViewPresenter) {
+        this.iViewPresenter = iViewPresenter;
+
+        passwordModel = new PasswordModel();
+    }
+
+
+    public void evaluatePassword(String password) {
+        int level = passwordModel.validatePassword(password);
+        if (level == PasswordModel.WEAK) {
+            this.iViewPresenter.showWeakPassword();
+
+        } else if (level == PasswordModel.MEDIUM) {
+            this.iViewPresenter.showMediumPassword();
+
+        } else if (level == PasswordModel.STRONG) {
+            this.iViewPresenter.showStrongPassword();
+
+        } else {
+            this.iViewPresenter.showErrorMessage();
+
+        }
+    }
+}
